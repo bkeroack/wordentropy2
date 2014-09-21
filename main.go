05160e6b7454a691a7f64b69ab1c37ff83ce405b
Benjamen Keroack <ben@keroack.com>
2014-09-20 19:21:03 -0700

    initial stubs

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/eknkc/amber"
+	"log"
+	"net/http"
+)
+
+var TLS_CERT = "tls/cert-unified.pem"
+var TLS_KEY = "tls/cert.key"
+
+func main() {
+	log.Printf("Starting and listening on 4343")
+	http.HandleFunc("/", Root)
+	http.ListenAndServeTLS("0.0.0.0:4343", TLS_CERT, TLS_KEY, nil)
+}
+
+func Root(w http.ResponseWriter, r *http.Request) {
+	compiler := amber.New()
+	err := compiler.ParseFile("views/main.amber")
+	if err != nil {
+		http.Error(w, "Bad template: main.amber", 500)
+	}
+	tpl, err := compiler.Compile()
+	if err != nil {
+		http.Error(w, "Error compiling template: main.amber", 500)
+	}
+	tpl.Execute(w, nil)
+}
