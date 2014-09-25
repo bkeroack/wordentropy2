e4dbae4398cf2a03ca92f33992b6635bc4516bf2
Benjamen Keroack <ben@keroack.com>
2014-09-25 08:32:53 -0700

    word map loading works, still a bug with pos detection

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-package generate
+package main
 
 import (
 	"bufio"
@@ -9,10 +9,35 @@ import (
 
 var WORDNET_PATH = "part-of-speech.txt"
 
+// word_type -> "can be followed by..."
+var GRAMMAR_RULES = map[string][]string{
+	"snoun":       []string{"adverb", "verb", "pronoun", "conjunction"},
+	"pnoun":       []string{"adverb", "verb", "pronoun", "conjunction"},
+	"verb":        []string{"snoun", "pnoun", "preposition", "adjective", "conjunction", "sarticle", "particle"},
+	"adjective":   []string{"snoun", "pnoun"},
+	"adverb":      []string{"verb"},
+	"preposition": []string{"snoun", "pnoun", "adverb", "adjective", "verb"},
+	"pronoun":     []string{"verb", "adverb", "conjunction"},
+	"conjunction": []string{"snoun", "pnoun", "pronoun", "verb", "sarticle", "particle"},
+	"sarticle":    []string{"snoun", "adjective"},
+	"particle":    []string{"pnoun", "adjective"},
+}
+
 //Load Wordnet into a mapping of word type to words of that type
 func LoadWordMap() map[string][]string {
 
-	word_map := make(map[string][]string)
+	word_map := map[string][]string{
+		"snoun":       make([]string, 1),
+		"pnoun":       make([]string, 1),
+		"verb":        make([]string, 1),
+		"adjective":   make([]string, 1),
+		"adverb":      make([]string, 1),
+		"preposition": make([]string, 1),
+		"pronoun":     make([]string, 1),
+		"conjunction": make([]string, 1),
+		"sarticle":    make([]string, 1),
+		"particle":    make([]string, 1),
+	}
 
 	file, err := os.Open(WORDNET_PATH)
 	if err != nil {
@@ -22,10 +47,11 @@ func LoadWordMap() map[string][]string {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		word_type := ""
 		plural := false
 		line := scanner.Text()
-		line_array := strings.Split(line, '\t')
-		if len(line_array) != 2{
+		line_array := strings.Split(line, "\t")
+		if len(line_array) != 2 {
 			log.Printf("Bad string array length: %v, string: %v", len(line_array), line)
 			continue
 		}
@@ -36,6 +62,35 @@ func LoadWordMap() map[string][]string {
 				plural = true
 			}
 		}
-		if val, ok := 
+		if strings.Contains(pos_tag, "D") || strings.Contains(pos_tag, "I") {
+			if plural {
+				word_type = "particle"
+			} else {
+				word_type = "sarticle"
+			}
+		} else if strings.Contains(pos_tag, "N") || strings.Contains(pos_tag, "h") || strings.Contains(pos_tag, "o") {
+			if plural {
+				word_type = "pnoun"
+			} else {
+				word_type = "snoun"
+			}
+		} else if strings.Contains(pos_tag, "V") || strings.Contains(pos_tag, "t") || strings.Contains(pos_tag, "i") {
+			word_type = "verb"
+		} else if strings.Contains(pos_tag, "A") {
+			word_type = "adjective"
+		} else if strings.Contains(pos_tag, "v") {
+			word_type = "adverb"
+		} else if strings.Contains(pos_tag, "C") {
+			word_type = "conjunction"
+		} else if strings.Contains(pos_tag, "P") {
+			word_type = "preposition"
+		} else if strings.Contains(pos_tag, "r") {
+			word_type = "pronoun"
+		} else {
+			log.Printf("Unknown word type! word: %v; pos: %v\n", word, pos_tag)
+			continue
+		}
+		word_map[word_type] = append(word_map[word_type], word)
 	}
+	return word_map
 }
--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"github.com/eknkc/amber"
 	"log"
 	"net/http"
-	"wordentropy2/generate"
 )
 
 var TLS_CERT = "tls/cert-unified.pem"
 var TLS_KEY = "tls/cert.key"
 
+var word_map = map[string][]string{}
+
 func main() {
+	log.Printf("Loading word map")
+	word_map = LoadWordMap()
 	log.Printf("Starting and listening on 4343")
 	http.HandleFunc("/", Root)
 	http.ListenAndServeTLS("0.0.0.0:4343", TLS_CERT, TLS_KEY, nil)
